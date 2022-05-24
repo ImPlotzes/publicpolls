3326f82c5a745c67a5cc11e2d03b82041a2b548a
ImPlotzes <47643346+ImPlotzes@users.noreply.github.com>
2022-05-25 01:04:26 +0200

    Added meta tags and dynamic generation of them for poll pages

--- pages/functions/poll/[poll].js
+++ pages/functions/poll/[poll].js
@@ -1,14 +1,45 @@
+let poll = null;
+
 /**
  * Responds with the same HTML to requests to all /poll/xxxxx routes
  * @param {*} context Object containing request, enviroment and other bindings
  */
 export async function onRequestGet(context) {
+    // Get the origin
     const url = new URL(context.request.url);
     const origin = url.origin;
+
+    // Get poll information for the meta tags
+    const pollID = url.pathname.replace("/poll/", "");
+    const apiRes = await fetch(origin + "/api/poll?id=" + pollID);
+    poll = await apiRes.json();
+
     const res = await fetch(origin + "/assets/html/poll");
-    return new Response(res.body, {
-        headers: {
-            "Content-Type": "text/html"
+
+    // Rewrite the HTML to dynamically fill in the meta tags
+    return new HTMLRewriter().on("meta", new ElementHandler()).transform(res);
+}
+
+
+/**
+ * Handles incoming HTML elements for the HTMLRewriter
+ */
+class ElementHandler {
+    /**
+     * Dynamically edits the meta tags in the HTML
+     * @param {*} element An incoming meta HTML tag
+     */
+    element(element) {
+        // Set the title meta tags to the poll question.
+        if(element.getAttribute("name")?.includes("title")
+        || element.getAttribute("property")?.includes("title")) {
+            element.setAttribute("content", poll.question);
         }
-    });
+
+        // Set the description meta tags to the poll options
+        if(element.getAttribute("name")?.includes("description")
+        || element.getAttribute("property")?.includes("description")) {
+            element.setAttribute("content", poll.options.map(o => o.value).join(" | "));
+        }
+    }
 }
