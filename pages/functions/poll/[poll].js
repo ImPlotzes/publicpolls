8cbe5a893a8836b4a8da4ff36ee71930b2363e7b
ImPlotzes <47643346+ImPlotzes@users.noreply.github.com>
2022-05-25 16:37:40 +0200

    Optimized and changed the dynamic poll meta tags

--- pages/functions/poll/[poll].js
+++ pages/functions/poll/[poll].js
@@ -1,5 +1,5 @@
 let POLL = null;
-//const WORKER_DEV_DOMAIN = "http://localhost:8787";
+let DESCRIPTION = null;
 const WORKER_DEV_DOMAIN = "https://publicpolls.plotzes.workers.dev";
 
 /**
@@ -11,27 +11,73 @@ export async function onRequestGet(context) {
     const url = new URL(context.request.url);
     const origin = url.origin;
 
-    // Get poll information for the meta tags
     const pollID = url.pathname.replace("/poll/", "");
-    const apiRes = await fetch(WORKER_DEV_DOMAIN + "/api/poll?id=" + pollID, {
-        headers: {
-            "X-User-IP": context.request.headers.get("CF-Connecting-IP")
-        }
-    });
 
-    // If the poll couldn't be fetched, return normal response
-    if(apiRes.status != 200) {
-        const res = await fetch(origin + "/assets/html/poll");
-        return new Response(res.body, {
+    const promises = [];
+
+    // Get poll information for the meta tags
+    promises.push((async () => {
+        const apiRes = await fetch(WORKER_DEV_DOMAIN + "/api/poll?id=" + pollID, {
+            headers: {
+                "X-User-IP": context.request.headers.get("CF-Connecting-IP")
+            }
+        });
+        
+        // Throw an error to reject this promise if the API call failed
+        if(apiRes.status != 200) {
+            throw new Error("API call failed");
+        }
+
+        // Return the JSON response
+        return apiRes.json();
+    })());
+
+    // Get the HTML for the page
+    promises.push(fetch(origin + "/assets/html/poll"));
+
+    // Wait for all the promises to resolve
+    const [poll, html] = await Promise.allSettled(promises);
+
+    // If the API call failed, return the normal HTML page
+    if(poll.status != "fulfilled") {
+        return new Response(html.value.body, {
             headers: {
                 "Content-Type": "text/html"
             }
         });
     }
+    // The API call was successful, so we return the rewritten HTML
 
-    POLL = await apiRes.json();
+    POLL = poll.value;
+    const res = html.value;
 
-    const res = await fetch(origin + "/assets/html/poll");
+    // Create the description
+    let date = "Posted ";
+    const diff = Date.now() - POLL.created_at;
+
+    // Show relative hours and minutes if the poll was posted less than 24h ago
+    if(diff < 86400000) {
+        const hours = Math.floor(diff / 3600000);
+        if(hours > 0) {
+            date += `${hours} hour${hours == 1 ? "" : "s"} and `;
+        }
+        const minutes = Math.floor((diff % 3600000) / 60000);
+        date += `${minutes} minute${minutes == 1 ? "" : "s"} ago`;
+
+    // Show relative days and hours if the poll was posted less than a week ago
+    } else if(diff < 604800000) {
+        const days = Math.floor(diff / 86400000);
+        date += `${days} day${days == 1 ? "" : "s"}`;
+        const hours = Math.floor((diff % 86400000) / 3600000);
+        date += ` and ${hours} hour${hours == 1 ? "" : "s"} ago`;
+
+    // Show absolute date if the poll was posted more than a week ago
+    } else {
+        const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        const dateObj = new Date(POLL.created_at);
+        date += `${months[dateObj.getMonth()]} ${dateObj.getDate()}, ${dateObj.getFullYear()}`;
+    }
+    DESCRIPTION = "Asked by " + POLL.name + " | " + date;
 
     // Rewrite the HTML to dynamically fill in the meta tags
     return new HTMLRewriter().on("meta", new ElementHandler()).transform(res);
@@ -61,7 +107,7 @@ class ElementHandler {
         // Set the description meta tags to the poll options
         if(element.getAttribute("name")?.includes("description")
         || element.getAttribute("property")?.includes("description")) {
-            element.setAttribute("content", POLL.options.map(o => o.value).join(" | "));
+            element.setAttribute("content", DESCRIPTION);
         }
     }
 }
